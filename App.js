e78b6301f931cb607af3e20236bd487e9ba68370
congdanh1252 <70893956+congdanh1252@users.noreply.github.com>
2021-09-16 20:07:15 +0700

    Add RNElements

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import { SocialIcon } from 'react-native-elements/dist/social/SocialIcon';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app</Text>
+      <SocialIcon
+        style={{width: 200}}
+        title='Sign In With Facebook'
+        button
+        type='facebook'
+      />
     </View>
   );
 }
