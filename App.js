d33ef493297a86996a41dfef4903166e589d8b81
congdanh1252 <70893956+congdanh1252@users.noreply.github.com>
2021-09-17 21:26:15 +0700

    Add 4 screens
    Add 4 screens:
    + Welcome screen
    + Register screen 1
    + Register screen 2
    + Register screen 3

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native'
 import { SocialIcon } from 'react-native-elements/dist/social/SocialIcon';
+import { RegisterScreen } from './src/screens/RegisterScreen_3';
+import { WelcomeScreen } from './src/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app</Text>
-      <SocialIcon
-        style={{width: 200}}
-        title='Sign In With Facebook'
-        button
-        type='facebook'
-      />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app</Text>
+    //   <SocialIcon
+    //     style={{width: 200}}
+    //     title='Sign In With Facebook'
+    //     button
+    //     type='facebook'
+    //   />
+    // </View>
+    <RegisterScreen/>
   );
 }
 
