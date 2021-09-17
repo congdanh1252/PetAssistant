e63093241c1692a243e712ef7d9fd55cd64362e1
DucDung_Tong <56964889+ducdung-tong@users.noreply.github.com>
2021-09-17 09:16:14 +0700

    Add Folder, fonts, colors, icons

---
+++ src/theme/colors.js
@@ -0,0 +1,15 @@
+  
+const COLORS = {
+    primary: '#E5CD8E',
+    primaryDark: '#A77B06',
+
+    white: '#fff',
+    blue: '#5DA3FA',
+};
+  
+  export default COLORS;
+
+/* 
+    USE COLOR FROM https://uicolorpicker.com/
+    PLEASE CHECK IF THE COLOR EXISTED, DON'T ADD DUPLICATE COLORS 
+*/
